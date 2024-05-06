8b012a4a026877931b3324d1d3657cb1630f08ea
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-05-06 14:21:23 +0300

    add constants to our project

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+const String apiKey="6567ad004d574d349137006a42cde22e";
+
+
+const Color kPrimaryColor=Color(0xFFE23E3E);
+const Color kWhiteColor=Color(0xFFffffff);
+
+const String appLogo="assets/images/logo.png";
+const String welcomeImage="assets/images/welcome.png";
