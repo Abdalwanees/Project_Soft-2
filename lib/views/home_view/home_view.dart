91be9b386f184452a1be2233a45e9d996075da84
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-05-06 14:42:54 +0300

    add body for home screen

--- lib/views/home_view/home_view.dart
+++ lib/views/home_view/home_view.dart
@@ -7,6 +7,8 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double widthScreen = MediaQuery.of(context).size.width;
+
     return SafeArea(
       child: Scaffold(
           appBar: AppBar(
@@ -27,7 +29,29 @@ class HomeView extends StatelessWidget {
             centerTitle: true,
             automaticallyImplyLeading: false,
           ),
-          body: const Column()),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: ListView(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              physics: const BouncingScrollPhysics(),
+              children: [
+
+              // here today recipe widget
+                Padding(
+                  padding: const EdgeInsetsDirectional.only(bottom: 16),
+                  child: Text(
+                    "Categories",
+                    style: TextStyle(
+                      fontSize: widthScreen / 15,
+                      fontWeight: FontWeight.w500,
+                      color: const Color(0xFF323643),
+                    ),
+                  ),
+                ),
+               // here category list view
+              ],
+            ),
+          ),),
     );
   }
 }
