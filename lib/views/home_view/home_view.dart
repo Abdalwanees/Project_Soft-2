8e6b5141f1af4bb20a04975a0a671d3c2022ce5b
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-05-06 14:41:32 +0300

    create home screen and add app bar

---
+++ lib/views/home_view/home_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "RECIPE",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  " APP",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: kPrimaryColor),
+                ),
+              ],
+            ),
+            centerTitle: true,
+            automaticallyImplyLeading: false,
+          ),
+          body: const Column()),
+    );
+  }
+}
