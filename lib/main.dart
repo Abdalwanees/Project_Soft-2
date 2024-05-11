9cc88be7fd17cce9c80ac87b3444b965453ed49a
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-05-11 22:05:49 +0300

    add home view ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:softwareproject/views/home_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,9 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(child: Text("MAIN")),
-      ),
+      home: const HomeView()
     );
   }
 }
