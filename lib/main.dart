ef65b0b84e41e510706822a18fc35863e8156dd1
MAHMOUD <mahmoud.alaa1612@gmail.com>
2024-05-15 16:12:56 +0300

    make welcome view the next screen after display splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:softwareproject/constants.dart';
 import 'package:softwareproject/services/all_categories_services.dart';
 import 'package:softwareproject/services/get_recipe_by_id.dart';
 import 'package:softwareproject/services/get_recipe_in_one_category.dart';
-import 'package:softwareproject/views/home_view.dart';
+import 'package:softwareproject/views/welcome_view.dart';
 
 import 'cubits/category_cubit/category_cubit.dart';
 import 'cubits/recipe_cubit/recipe_cubit.dart';
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
           animationDuration:const Duration(seconds: 1),
           splash: Constants.appLogo,
           splashIconSize: widthScreen/1.5,
-          nextScreen: const HomeView(),
+          nextScreen: const WelcomeView(),
           splashTransition: SplashTransition.fadeTransition,
           curve: Curves.decelerate,
           backgroundColor: Colors.black,
